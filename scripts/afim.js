0271b6d5e5421248544ff0cb44c93e62ae3abc32
Isaias Santos Silva <isaias.silva.dev@gmail.com>
2026-02-28 23:03:28 -0300

    grafico afim canvas

--- scripts/afim.js
+++ scripts/afim.js
@@ -217,6 +217,129 @@ function realizarCalculoAfim() {
   }
 }
 
+function gradesGraficoAfuim(cx, cy, escala) {
+  const caixaGrafico = document.querySelector('#grafico')
+  const canvas = document.getElementById('desenho');
+  const ctx = canvas.getContext('2d');
+
+
+  const lX = Math.ceil(canvas.width / (2 * escala))
+  const lY = Math.ceil(canvas.height / (2 * escala))
+
+  ctx.strokeStyle = "#ddd"
+  ctx.lineWidth = 1
+
+  //linhas verticais
+  for (let i = -lX; i <= lX; i++) {
+    let xCanvas = cx + i * escala
+
+    ctx.beginPath()
+    ctx.moveTo(xCanvas, 0)
+    ctx.lineTo(xCanvas, canvas.height)
+    ctx.stroke()
+  }
+
+  //linhas Horizintais
+  for (let j = -lY; j <= lY; j++) {
+    let yCanvas = cy - j * escala
+
+    ctx.beginPath()
+    ctx.moveTo(0, yCanvas)
+    ctx.lineTo(canvas.width, yCanvas)
+    ctx.stroke()
+  }
+}
+
+//Desenhar Grafico
+function desenharGraficoAfim(a, b) {
+  const caixaGrafico = document.querySelector('#grafico')
+
+  const canvas = document.getElementById('desenho');
+  const ctx = canvas.getContext('2d');
+
+  ctx.beginPath()
+  ctx.rect(0, 0, canvas.width, canvas.height)
+  ctx.clip()
+
+  caixaGrafico.classList.remove('invisivel')
+
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  const centroX = canvas.width / 2;
+  const centroY = canvas.height / 2
+  const escala = 20
+
+  gradesGraficoAfuim(centroX, centroY, escala)
+
+  ctx.strokeStyle = "black"
+  ctx.lineWidth = 2
+
+  //Eixo X
+  ctx.beginPath()
+  ctx.moveTo(0, centroY)
+  ctx.lineTo(canvas.width, centroY)
+  ctx.stroke()
+
+  //Eixo Y
+  ctx.beginPath()
+  ctx.moveTo(centroX, 0)
+  ctx.lineTo(centroX, canvas.height)
+  ctx.stroke()
+
+  //Numeros nos eixos
+  ctx.font = "12px Arial"
+  ctx.fillStyle = "black"
+
+  //Numeros X
+
+  const limiteX = Math.floor(canvas.width / (2 * escala))
+  
+  for (let i = -limiteX; i <= limiteX; i++) {
+    ctx.fillText(i, centroX + i * escala - 5, centroY + 15)
+  }
+
+  //Numeros Y
+
+  const limiteY = Math.floor(canvas.height / (2 * escala))
+
+  for (let i = -limiteY; i <= limiteY; i++) {
+    ctx.fillText(i, centroX + 5, centroY - i * escala + 4)
+  } 
+
+  ctx.strokeStyle = "blue";
+  ctx.lineWidth = 2
+
+  ctx.beginPath()
+
+  for (let x = -limiteX; x <= limiteX; x+= 0.1) {
+    let y = a * x + b
+
+    let xCanvas = centroX + x * escala
+
+    let yCanvas = centroY - y * escala
+
+    if (x === -limiteX) {
+      ctx.moveTo(xCanvas, yCanvas)
+    } else {
+      ctx.lineTo(xCanvas, yCanvas)
+    }
+    
+  }
+
+  ctx.stroke()
+
+  centralizarVizuGrafico()
+
+}
+
+function centralizarVizuGrafico() {
+  const caixaGrafico = document.querySelector('#grafico')
+
+  caixaGrafico.scrollLeft =  (caixaGrafico.scrollWidth - caixaGrafico.clientWidth) / 2;
+  caixaGrafico.scrollTop = (caixaGrafico.scrollHeight - caixaGrafico.clientHeight) / 2;
+}
+
 function validarCamposAfim(...valores) {
   for (let valor of valores) {
     if (isNaN(valor)) {
@@ -243,6 +366,8 @@ btnCalcAf.addEventListener('click', () => {
   }
 
   realizarCalculoAfim()
+
+  desenharGraficoAfim(a, b)
 })
 
 
