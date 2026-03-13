1bc9ce8e5775273c5fb318cfe9e8e87befcec806
Isaias Silva <isaias.silva.dev@gmail.com>
2026-03-13 20:22:27 -0300

    Update modo offline do app

--- service-worker.js
+++ service-worker.js
@@ -5,3 +5,28 @@ self.addEventListener('install', (event) => {
 self.addEventListener('fetch', (event) => {
   // intercepta requisições
 })
+
+const CACHE_NAME = "algebrando-cache-v1";
+
+const arquivos = [
+  "/",
+  "/index.html",
+  "/Styles/global.css",
+  "/scripts/main.js"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(arquivos);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
